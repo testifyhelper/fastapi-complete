24f91e65b76e02bbbc03f285184814b4d3096178
Testify <testify876@gmail.com>
2023-04-09 07:01:53 +0500

    my fourth  push

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,17 +1,6 @@
 import pytest
 from app import schemas
 
-
-def test_unauthorized_user_get_all_posts(client, test_posts):
-    res = client.get("/posts/")
-    assert res.status_code == 401
-
-
-def test_unauthorized_user_get_one_post(client, test_posts):
-    res = client.get(f"/posts/{test_posts[0].id}")
-    assert res.status_code == 401
-
-
 def test_get_one_post_not_exist(authorized_client, test_posts):
     res = authorized_client.get(f"/posts/88888")
     assert res.status_code == 404
